f814725c0198b2d3e20b198fc021f15b168ed87d
vishalmet <vishalmet003@gmail.com>
2024-09-06 21:13:54 +0530

    buttons comp

---
+++ src/modals/Buttons.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+
+const Buttons = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const handleEnableClick = () => {
+    setIsEnabled(true);
+  };
+
+  return (
+    <div className="space-y-2">
+      {!isEnabled && (
+        <motion.button
+          className="bg-[#1FC7D4] hover:bg-[#1FC7D4]/50 transition-colors duration-200 p-2 w-full rounded-xl text-black font-bold"
+          whileTap={{ scale: 0.9 }}
+          onClick={handleEnableClick}
+        >
+          Enable
+        </motion.button>
+      )}
+
+      {isEnabled && (
+        <>
+          <motion.button
+            className="bg-[#1FC7D4] hover:bg-[#1FC7D4]/50 transition-colors duration-200 p-2 w-full rounded-xl text-black font-bold"
+            whileTap={{ scale: 0.9 }}
+            // onClick={switchToModal2} 
+          >
+            Buy Instantly
+          </motion.button>
+
+          <motion.button
+            className="border-2 border-[#1FC7D4] hover:border-[#1FC7D4]/80 text-[#1FC7D4] hover:text-[#1FC7D4]/80 transition-colors duration-200 p-2 w-full rounded-xl font-bold"
+            whileTap={{ scale: 0.9 }}
+          >
+            View/Edit Numbers
+          </motion.button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Buttons;
