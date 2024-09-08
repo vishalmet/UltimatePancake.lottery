ad504d5509a998050ecf76f1527267ea20ba24e5
vishalmet <vishalmet003@gmail.com>
2024-09-08 08:45:47 +0530

    modal switching

--- src/modals/Buttons.jsx
+++ src/modals/Buttons.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-const Buttons = () => {
+const Buttons = ({ switchToModal2 }) => { // Receive the function as a prop
   const [isEnabled, setIsEnabled] = useState(false);
 
   const handleEnableClick = () => {
@@ -25,7 +25,7 @@ const Buttons = () => {
           <motion.button
             className="bg-[#1FC7D4] hover:bg-[#1FC7D4]/50 transition-colors duration-200 p-2 w-full rounded-xl text-black font-bold"
             whileTap={{ scale: 0.9 }}
-            // onClick={switchToModal2} 
+            onClick={switchToModal2} // Switch to Modal2 when clicked
           >
             Buy Instantly
           </motion.button>
@@ -33,6 +33,7 @@ const Buttons = () => {
           <motion.button
             className="border-2 border-[#1FC7D4] hover:border-[#1FC7D4]/80 text-[#1FC7D4] hover:text-[#1FC7D4]/80 transition-colors duration-200 p-2 w-full rounded-xl font-bold"
             whileTap={{ scale: 0.9 }}
+            onClick={switchToModal2} // Switch to Modal2 when clicked
           >
             View/Edit Numbers
           </motion.button>
